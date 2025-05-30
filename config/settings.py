b72a3520f8fdc94d0f1b74ca1c5b03e63eeaf6df
sina hozhabri <sinahzdev@gmail.com>
2025-05-31 01:31:17 +0330

    add rest frame work and django toolbars too the project

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # django toolbars app
+    "debug_toolbar",
+    # drf app
+    'rest_framework',
     #local apps
     'store',
 ]
@@ -49,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # django toolbaer middleware
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'config.urls'
