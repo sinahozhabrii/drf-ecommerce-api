6bc9b025dc1cffea9f3d7f7aecf232efb8863ff5
sina hozhabri <sinahzdev@gmail.com>
2025-07-18 00:44:44 +0330

    fix bog

--- config/settings.py
+++ config/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY',default=False,cast=bool)
+SECRET_KEY = config('SECRET_KEY',default=False,)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG',default=False)
+DEBUG = config('DEBUG',default=False,cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
