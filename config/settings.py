73fbf959725e0178bff158f1982a14fcacbe8e0d
sina hozhabri <sinahzdev@gmail.com>
2025-07-19 00:40:03 +0330

    changed middleware

--- config/settings.py
+++ config/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -67,7 +68,6 @@ MIDDLEWARE = [
     # django toolbaer middleware
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     #whitenoise
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
